22f94491b6a349254a35eb519410533e14857a50
Sk Mursalin <skmursalin02@gmail.com>
2025-09-03 20:54:27 +0530

    sort an array

--- practice.js
+++ practice.js
@@ -304,6 +304,25 @@ function reverse() {
     }
     return arr
 }
-
 console.log(reverse());
 
+// # 🧠 Question 17  (New):
+// sort  an array 
+
+function sort() {
+    let arr = [5, 2, 4, 3, 1]
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            }
+        }
+    }
+    return arr
+
+}
+
+console.log(sort());
